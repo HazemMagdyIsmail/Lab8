ebfda9006e8861df856ebae76cf9b8cfaab68dda
Abdelrahman-711 <abdelrahmanharidy90@gmail.com>
2025-11-22 00:26:13 +0200

    Start
    Pushing Lab 7

---
+++ src/utils/IdGenerator.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+
+import java.util.UUID;
+
+public class IdGenerator {
+
+    public static String generateUserId() {
+        return "USER_" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
+
+
+    public static String generateId(String prefix) {
+        return prefix + "_" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
+    }
+
+    public static String generateSimpleId() {
+        return String.valueOf(System.currentTimeMillis());
+    }
+}
