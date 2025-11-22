beb4f96f75dd2b34788f0161ea7ddee1e27af5e6
Abdelrahman-711 <abdelrahmanharidy90@gmail.com>
2025-11-22 23:34:16 +0200

    Create Question.java

---
+++ src/models/Question.java
@@ -0,0 +1,53 @@
+package models;
+
+import java.util.List;
+
+public class Question {
+    private int questionId;
+    private String questionText;
+    private List<String> options;
+    private int correctAnswerIndex;
+
+    public Question(int questionId, String questionText, List<String> options, int correctAnswerIndex) {
+        this.questionId = questionId;
+        this.questionText = questionText;
+        this.options = options;
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public int getCorrectAnswerIndex() {
+        return correctAnswerIndex;
+    }
+
+    public void setCorrectAnswerIndex(int correctAnswerIndex) {
+        this.correctAnswerIndex = correctAnswerIndex;
+    }
+
+    public boolean isCorrectAnswer(int answerIndex) {
+        return answerIndex == correctAnswerIndex;
+    }
+}
